042cee12c5a6cb7e284c14d71bd1f1601eff4ef0
jcpyun <johnchpyun@gmail.com>
2016-11-28 13:09:25 -0500

    news update

--- news.js
+++ news.js
@@ -1,19 +1,57 @@
-xmlhttp=new XMLHttpRequest();
-xmlhttp.open("GET", "https://newsapi.org/v1/articles?source=bloomberg&sortBy=top&apiKey=8a284b7114cc49efa3f55b969b850f9a", false);
-xmlhttp.send();
-var parseddata = JSON.parse(xmlhttp.responseText);
-console.log(parseddata);
+
 function bloombergNews(){
-    document.getElementById("bloomberg").innerHTML = parseddata.articles;
-    // for (var i=0; i<parseddata.articles.length;i++){
-    //     console.log(parseddata.articles[i].title);
-    //     var newstitle;
-    //     newstitle+= parseddata.articles[i].title;
-    //     document.getElementById("bloomberg").innerHTML = newstitle;
-    // }
+  xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET", "https://newsapi.org/v1/articles?source=bloomberg&sortBy=top&apiKey=8a284b7114cc49efa3f55b969b850f9a", false);
+  xmlhttp.send();
+  var parseddata = JSON.parse(xmlhttp.responseText);
+
+  var titles=[];
+  var newDiv= document.createElement("div");
+  var br = document.createElement("br");
+  var output="";
+  for (var i=0; i<parseddata.articles.length;i++){
+    titles.push(parseddata.articles[i].title);
+    output += parseddata.articles[i].title + "<br>";
+    document.getElementById("bloomberg").innerHTML = output;
+  }
 }
+function BBCNews(){
+  xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET", "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=8a284b7114cc49efa3f55b969b850f9a", false);
+  xmlhttp.send();
+  var parseddata = JSON.parse(xmlhttp.responseText);
+
+  var titles=[];
+  var newDiv= document.createElement("div");
+  var br = document.createElement("br");
+  var output="";
+  for (var i=0; i<parseddata.articles.length;i++){
+    titles.push(parseddata.articles[i].title);
+    output += parseddata.articles[i].title + "<br>";
+    document.getElementById("bbc").innerHTML = output;
+  }
+}
+function CNNNews(){
+  xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET", "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=8a284b7114cc49efa3f55b969b850f9a", false);
+  xmlhttp.send();
+  var parseddata = JSON.parse(xmlhttp.responseText);
+
+  var titles=[];
+  var newDiv= document.createElement("div");
+  var br = document.createElement("br");
+  var output="";
+  for (var i=0; i<parseddata.articles.length;i++){
+    titles.push(parseddata.articles[i].title);
+    output += parseddata.articles[i].title + "<br>";
+    document.getElementById("bbc").innerHTML = output;
+  }
+}
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
   bloombergNews();
+  BBCNews();
 
 });
